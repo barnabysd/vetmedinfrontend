c07d54f33e842901cfc1f6212b89e33b596de0ad
Barnaby Menage <barnabym@gmail.com>
2020-04-22 12:48:23 +0100

    tag manager

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,89 +1,39 @@
 var quizQuestions = [
   {
-      question: "What franchise would you rather play a game from?",
+      question: "Which dog toy is best?",
       answers: [
           {
-              type: "Microsoft",
-              content: "Halo"
+              type: "Pug",
+              content: "Bone"
           },
           {
-              type: "Nintendo",
-              content: "Pokemon"
+              type: "Rottweiller",
+              content: "Squeaky Toy"
           },
           {
-              type: "Sony",
-              content: "Uncharted"
+              type: "Labrador",
+              content: "slipper"
           }
       ]
   },
   {
-      question: "Which console would you prefer to play with friends?",
+      question: "Which dog trick is best?",
       answers: [
           {
-              type: "Microsoft",
-              content: "X-Box"
+              type: "Pug",
+              content: "Roll over"
           },
           {
-              type: "Nintendo",
-              content: "Nintendo 64"
+              type: "Rottweiller",
+              content: "Sit"
           },
           {
-              type: "Sony",
-              content: "Playstation 1"
-          }
-      ]
-  },
-  {
-      question: "Which of these racing franchises would you prefer to play a game from?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "Forza"
-          },
-          {
-              type: "Nintendo",
-              content: "Mario Kart"
-          },
-          {
-              type: "Sony",
-              content: "Gran Turismo"
-          }
-      ]
-  },
-  {
-      question: "Which of these games do you think is best?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "BioShock"
-          },
-          {
-              type: "Nintendo",
-              content: "The Legend of Zelda: Ocarina of Time"
-          },
-          {
-              type: "Sony",
-              content: "Final Fantasy VII"
-          }
-      ]
-  },
-  {
-      question: "What console would you prefer to own?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "X-Box One"
-          },
-          {
-              type: "Nintendo",
-              content: "Wii U"
-          },
-          {
-              type: "Sony",
-              content: "Playstation 4"
+              type: "Labrador",
+              content: "Jump"
           }
       ]
   }
+  
 ];
 
 export default quizQuestions;
