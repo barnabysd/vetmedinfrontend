59101ef20d17c5d720896aa0d423656d8d14d94e
Barnaby Menage <barnabym@gmail.com>
2020-08-02 21:12:24 +0100

    fixes after review 6

--- src/utils/assetUtils.js
+++ src/utils/assetUtils.js
@@ -139,8 +139,7 @@ export const getOwnerImageName = (dogState = animationCharacterState.NEUTRAL,dog
     }
 }
 //
-//
-//
+
 export const getDogImageName = (dogState = animationCharacterState.NEUTRAL,dogChoice) => {
     //debugger
     if (dogState === animationCharacterState.NEUTRAL) {
