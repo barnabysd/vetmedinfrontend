70b25102e1bf73a778adfc4ce05f511c809571e1
Ben Harvey <ben.harvey29@gmail.com>
2020-08-12 21:18:02 +0100

    Make the select field use the same border colour as the text fields, make the text fields fill the same space when validation applied

--- src/components/BorderSelect.js
+++ src/components/BorderSelect.js
@@ -6,6 +6,31 @@ import MenuItem from '@material-ui/core/MenuItem'
 import FormControl from '@material-ui/core/FormControl'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import theme, { sm, md, lg, xl } from '../theme'
+import { ThemeProvider } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  root: {
+
+  },
+  select: {
+    minWidth: '200px',
+    background: 'white',
+    borderColor: 'rgb(49, 173, 211)',
+    borderStyle:'solid',
+    borderWidth: '1px',
+    borderRadius: '4px',
+    paddingLeft: '24px',
+    paddingTop: '14px',
+    paddingBottom: '15px',
+    "&:focus":{
+      borderRadius: '4px',
+    },
+    icon: {
+      color: '#000',
+    }
+  }
+});
 
 const BorderSelect = ({id,name,currentValue, handleChange}) => {
   //const [val,setVal] = useState(1);
@@ -15,6 +40,8 @@ const BorderSelect = ({id,name,currentValue, handleChange}) => {
   //   };
 
   const borderSelectClasses = useBorderSelectStyles();
+
+  const classes = useStyles();
 
   // moves the menu below the select input
   const menuProps = {
@@ -40,34 +67,44 @@ const BorderSelect = ({id,name,currentValue, handleChange}) => {
   const customBorder = 'solid 1px ' + theme.palette.topazBlue.main
 
   return (
-    <FormControl style={{paddingLeft: '0.5rem',paddingTop: '0.5rem'}}>
+    <ThemeProvider theme={theme}>
+      <FormControl style={{
+        paddingLeft: '0.5rem',
+        paddingTop: '0.5rem',
+        width: 'calc(100% - 0.5rem)',
+      }}>
 
-      <Select
-        disableUnderline
-        classes={{ root: borderSelectClasses.select, }}
-        labelId={id}
-        IconComponent={iconComponent}
-        MenuProps={menuProps}
-        value={currentValue}
-        onChange={handleChange}
-        name={name}
-        style={{width: '400px' }}
-      >
-        <MenuItem value={""}>Please select</MenuItem>
-        <MenuItem value={"Independent"}>Independent</MenuItem>
-        <MenuItem value={"Linnaeus"}>Linnaeus</MenuItem>
-        <MenuItem value={"CVS"}>CVS</MenuItem>
-        <MenuItem value={"Medivet"}>Medivet</MenuItem>
-        <MenuItem value={"Goddards"}>Goddards</MenuItem>
-        <MenuItem value={"VetPartners"}>VetPartners</MenuItem>
-        <MenuItem value={"XL Vets"}>XL Vets</MenuItem>
-        <MenuItem value={"IVCE"}>IVCE</MenuItem>
-        <MenuItem value={"Vets4Pets / Companion Care (The Vet Group)"}>Vets4Pets / Companion Care (The Vet Group)</MenuItem>
-        <MenuItem value={"Student"}>Student</MenuItem>
-        <MenuItem value={"Other"}>Other</MenuItem>
+        <Select
+          disableUnderline
+          classes={{
+            root: classes.root,
+            select: classes.select,
+            icon: classes.icon,
+          }}
+          labelId={id}
+          IconComponent={iconComponent}
+          MenuProps={menuProps}
+          value={currentValue}
+          onChange={handleChange}
+          name={name}
+          style={{ }}
+        >
+          <MenuItem value={""}>Please select</MenuItem>
+          <MenuItem value={"Independent"}>Independent</MenuItem>
+          <MenuItem value={"Linnaeus"}>Linnaeus</MenuItem>
+          <MenuItem value={"CVS"}>CVS</MenuItem>
+          <MenuItem value={"Medivet"}>Medivet</MenuItem>
+          <MenuItem value={"Goddards"}>Goddards</MenuItem>
+          <MenuItem value={"VetPartners"}>VetPartners</MenuItem>
+          <MenuItem value={"XL Vets"}>XL Vets</MenuItem>
+          <MenuItem value={"IVCE"}>IVCE</MenuItem>
+          <MenuItem value={"Vets4Pets / Companion Care (The Vet Group)"}>Vets4Pets / Companion Care (The Vet Group)</MenuItem>
+          <MenuItem value={"Student"}>Student</MenuItem>
+          <MenuItem value={"Other"}>Other</MenuItem>
 
-      </Select>
-    </FormControl>
+        </Select>
+      </FormControl>
+    </ThemeProvider>
   );
 };
 
